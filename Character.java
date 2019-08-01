f8ee5d87ac43a4af83047a0e6d6d6506c6282a8b
Alex Messina <37945413+jampc3@users.noreply.github.com>
2019-08-01 14:53:28 -0500

    Update Character.java

--- Character.java
+++ Character.java
@@ -1,16 +1,20 @@
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Collections;
 
-public class Character implements Serializable{
+public class Character extends Save_Load implements Serializable{
     //Members
-    private int str;
-    private int con;
-    private int dex;
-    private int inte;
-    private int wis;
-    private int cha;
+    private int Str;
+    private int Con;
+    private int Dex;
+    private int Inte;
+    private int Wis;
+    private int Cha;
+    private ArrayList<Integer> attri = new ArrayList<>();
+    private ArrayList<Integer> attrimod = new ArrayList<>();
+    private ArrayList<Integer> save = new ArrayList<>();
 
-    //Member definer as a list
-    public void Character_Stats (int str, int con, int dex, int inte, int wis, int cha){
+    public Character(int str, int con, int dex, int inte, int wis, int cha){
         this.setStr(str);
         this.setCon(con);
         this.setDex(dex);
@@ -19,81 +23,117 @@ public class Character implements Serializable{
         this.setCha(cha);
     }
 
+    public Character(){
+
+    }
+
     //Getters and setters
     public void setStr(int str_value){
-        str = str_value;
+        this.Str = str_value;
     }
     public void setCon(int con_value){
-        this.con = con_value;
+        this.Con = con_value;
     }
     public void setDex(int dex_value){
-        this.dex = dex_value;
+        this.Dex = dex_value;
     }
     public void setWis(int wis_value) {
-        this.wis = wis_value;
+        this.Wis = wis_value;
 
     }
     public void setCha(int cha_value){
-        this.cha = cha_value;
+        this.Cha = cha_value;
     }
     public void setInte(int inte_value){
-        this.inte = inte_value;
+        this.Inte = inte_value;
     }
+    public void setCharacter(Character x){
+        Character A = x;
+    }
+
     public int getStr(){
-        return str;
+        return Str;
     }
     public int getCon(){
-        return this.con;
+        return Con;
     }
     public int getDex(){
-        return this.dex;
+        return Dex;
     }
     public int getCha(){
-        return this.cha;
+        return Cha;
     }
     public int getWis(){
-        return this.wis;
+        return Wis;
     }
     public int getInte(){
-        return this.inte;
+        return Inte;
     }
+    /*public Character getCharacter(){
+        return A;
+    }*/
+
+
+    public ArrayList<Integer> getsave() {
+        return save;
+    }
+
+    public ArrayList<Integer> getAttri() {
+        return attri;
+    }
+
+    public ArrayList<Integer> getAttrimod() {
+        return attrimod;
+    }
+
     public void sheet_calculations() {
-        int[] attri = {str, con, dex, inte, wis, cha};
-        int[] attrimod = {0, 1, 2, 3, 4, 5};
-        int[] save = {0, 1, 2, 3, 4, 5};
-        int attrilength = attri.length;
-        int modlength = attrimod.length;
-        String[] savetype = {"Physical", "Physical", "Evasion", "Evasion", "Mental", "Mental"};
+    attri.add(Str);
+    attri.add(Con);
+    attri.add(Dex);
+    attri.add(Inte);
+    attri.add(Wis);
+    attri.add(Cha);
 
-        /* Loop used to calculate and store attribute modfiers. Will also print out scores*/
-        for (int x = 0; x < attrilength; x++) {
-            if (attri[x] <= 3) {
-                attrimod[x] = -2;
-            }
-            if (attri[x] >= 4 && attri[x] <= 7) {
-                attrimod[x] = -1;
-            }
-            if (attri[x] >= 8 && attri[x] <= 13) {
-                attrimod[x] = 0;
-            }
-            if (attri[x] >= 14 && attri[x] <= 17) {
-                attrimod[x] = 1;
-            }
-            if (attri[x] >= 18) {
-                attrimod[x] = 2;
-            }//System.out.println ("Score: " + attri[x] + " Modifier    " + attrimod[x]);
+    System.out.println(attri.get(0));
 
+    /* Loop used to calculate and store attribute modfiers. Will also print out scores*/
+    for (int x = 0; x < attri.size(); x++) {
+        if (attri.get(x) <= 3) {
+            attrimod.add(-2);
         }
+        if (attri.get(x) >= 4 && attri.get(x) <= 7) {
+            attrimod.add(-1);
+        }
+        if (attri.get(x) >= 8 && attri.get(x) <= 13) {
+            attrimod.add(0);
+        }
+        if (attri.get(x) >= 14 && attri.get(x) <= 17) {
+            attrimod.add(1);
+        }
+        if (attri.get(x) >= 18) {
+            attrimod.add(2);
+        }
+        System.out.println(" Modifier    " + attrimod.get(x));
 
-        /* Loop used to calculate and store attribute saving throws. Will also print out scores */
-        for (int x = 0; x < modlength; x += 2) {
-            if (attrimod[x] > attrimod[x + 1]) {
-                save[x] = 15 - attrimod[x];
-            }
-            if (attrimod[x] < attrimod[x + 1]) {
-                save[x] = 15 - attrimod[x + 1];
-            } else save[x] = 15 - attrimod[x];
-            //System.out.println(savetype[x] + " save    " + save[x]);}
+    }
+
+    /* Loop used to calculate and store attribute saving throws. Will also print out scores */
+    for (int x = 0; x < attrimod.size(); x += 2) {
+        if (attri.get(x) > attri.get(x+1)) {
+            int y = 15 - attrimod.get(x);
+            System.out.println(y);
+            save.add(y);
+            System.out.println(save.get(0));
+        } else {
+            int y = 15 - attrimod.get(x+1);
+            save.add(y);
+            System.out.println(save.get(0));
+
+
         }
     }
 }
+
+
+}
+
